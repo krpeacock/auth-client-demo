f0e7e0a102f1489df35079b35a524cf4975b2c42
Kyle Peacock <kylpeacock@gmail.com>
2023-08-08 10:05:51 -0700

    pwd for release

--- release.sh
+++ release.sh
@@ -3,8 +3,10 @@ dfx build --network ic
 
 echo "Copying to dist"
 mkdir -p dist
-cp .dfx/ic/canisters/whoami.wasm dist/whoami.wasm
+cp .dfx/ic/canisters/whoami/whoami.wasm dist/whoami.wasm
 cp .dfx/ic/canisters/whoami/whoami.did dist/whoami.did
 
 cp .dfx/ic/canisters/auth_client_demo_assets/auth_client_demo_assets.wasm dist/auth_client_demo_assets.wasm
 cp .dfx/ic/canisters/auth_client_demo_assets/auth_client_demo_assets.did dist/auth_client_demo_assets.did
+
+echo "Done"
