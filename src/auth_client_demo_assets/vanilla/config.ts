f6e2f073c3cfcf7023b65143dc051035faf77b4f
Kaia Peacock <kaia@peacock.dev>
2025-04-04 13:06:45 -0700

    fixing config.js

--- src/auth_client_demo_assets/vanilla/config.ts
+++ src/auth_client_demo_assets/vanilla/config.ts
@@ -3,26 +3,21 @@ export const DAYS = BigInt(24) * BigInt(60) * BigInt(60) * BigInt(1000000000);
 export const HOURS = BigInt(60) * BigInt(60) * BigInt(1000000000);
 export const NANOSECONDS = BigInt(1000000000);
 
-export function getIdentityProvider(): string {
-  if (typeof window === "undefined") {
-    return "http://localhost:4943";
+export const getIdentityProvider = () => {
+  let idpProvider;
+  // Safeguard against server rendering
+  if (typeof window !== "undefined") {
+    const isLocal = process.env.DFX_NETWORK !== "ic";
+    // Safari does not support localhost subdomains
+    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    if (isLocal && isSafari) {
+      idpProvider = `http://localhost:4943/?canisterId=rdmx6-jaaaa-aaaaa-aaadq-cai`;
+    } else if (isLocal) {
+      idpProvider = `http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:4943`;
+    }
   }
-
-  const isLocal =
-    window.location.hostname === "localhost" ||
-    window.location.hostname.endsWith(".localhost");
-  const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
-  if (isLocal) {
-    return "http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:4943";
-  }
-
-  if (isSafari) {
-    return "https://rdmx6-jaaaa-aaaaa-aaadq-cai.ic0.app";
-  }
-
-  return "https://rdmx6-jaaaa-aaaaa-aaadq-cai.icp0.io";
-}
+  return idpProvider;
+};
 
 export const defaultOptions = {
   /**
